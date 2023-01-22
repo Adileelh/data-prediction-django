f430b0fabcb8fa583cc41bd8f57de3a08060d734
Adileelh <adile.elhaddik@gmail.com>
2023-01-22 09:03:51 +0100

    rajout des templates

---
+++ home/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+def index(request):
+    return HttpResponse("this is my home page")
+# Create your views here.
+def predict(request):
+    return render(request, "predict/predict.html")
+# Create your views here.
+def contact(request):
+    return HttpResponse("this is my contact page")
